ae8ffa20343fa18987908420bdf202cb67d09091
HugoF-Silva <hugo.fernandes@discente.ufg.br>
2025-05-24 04:00:42 -0300

    fix: pipped setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="face_parsing",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.7.0",
+        "opencv-python",
+        "numpy>=1.19.5",
+        "opencv-python>=4.5.3.56"
+    ],
+    author="zllrunning",
+    description="BiSeNet face parsing",
+)
